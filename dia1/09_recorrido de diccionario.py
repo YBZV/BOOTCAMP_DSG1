175e120c12a28c58a18d610c0e7be721c049bee7
Yessica Zegarra <yessizegarrav@hotmail.com>
2024-07-07 14:53:39 -0500

    ultimo cambio

--- dia1/09_recorrido de diccionario.py
+++ dia1/09_recorrido de diccionario.py
@@ -7,7 +7,13 @@ capitales = {
 #recorrido por claves
 for clave in capitales.keys():
     print(clave)
+print('*'*50)
 #recorrido por valores 
 for value in capitales.values():
     print(value)
-    
+#recorrido por clave valor
+for clave,valor in capitales.items():
+    print(f'la capital de {clave} es {valor}')
+#diccionary comprehension
+paises = {valor:clave for clave,valor in capitales.items()}
+print(paises)
