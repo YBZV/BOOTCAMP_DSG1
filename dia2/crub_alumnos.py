6c6b6b8e7fc1e6dc0dd8a526c6f322a558116b14
Yessica Zegarra <yessizegarrav@hotmail.com>
2024-07-07 17:11:34 -0500

    primer cambio

---
+++ dia2/crub_alumnos.py
@@ -0,0 +1,52 @@
+import os
+import tabulate
+import time
+lista_alumnos = [
+              {
+                  'nombre':'yessica zegarra',
+                  'email': 'yessizegarrav@hotmail.com',
+                  'celular':'954079097'
+              }
+              ]
+ANCHO =50
+opcion = 0
+while(opcion < 5):
+    os.system("clear")
+    print("="*ANCHO)
+    print(" " * 10 + "CRUD DE ALUMNOS")
+    print("="*ANCHO)
+    print("""
+          [1] REGISTRAR ALUMNO
+          [2] MOSTRAR ALUMNO
+          [3] ACTUALIZAR ALUMNO
+          [4] ELIMINAR ALUMNO 
+          [5] SALIR
+    """)
+    print("="*ANCHO)
+    opcion = int(input("INGRESE LA OPCION : "))
+    os.system("clear")
+    if(opcion == 1):
+        print("="*ANCHO)
+        print(" "*10 + "[1] REGISTRAR ALUMNO")
+        print("="*ANCHO)
+    elif (opcion == 2):
+        print("="*ANCHO)
+        print(" "*10 + "[2] MOSTRAR ALUMNO")
+        print("="*ANCHO)
+    elif (opcion == 3):
+        print("="*ANCHO)
+        print(" "*10 + "[3] ACTUALIZAR ALUMNO")
+        print("="*ANCHO)
+    elif (opcion == 4):
+        print("="*ANCHO)
+        print(" "*10 + "[4] ELIMINAR ALUMNO")
+        print("="*ANCHO)
+    elif (opcion == 5):
+        print("="*ANCHO)
+        print(" "*10 + "[5] SALIR")
+        print("="*ANCHO)
+    else:
+        print("="*ANCHO)
+        print(" "*10 + "OPCION INVALIDA")
+        print("="*ANCHO)
+    time.sleep(1)
