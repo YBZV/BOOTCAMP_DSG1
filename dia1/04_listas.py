edaffb5be6fc97d6707b7a1610f9c8cbffac756e
Yessica Zegarra <yessizegarrav@hotmail.com>
2024-07-07 12:19:12 -0500

    segundo cambio

---
+++ dia1/04_listas.py
@@ -0,0 +1,10 @@
+dias = ["lunes", "martes", "miercoles"]
+print (dias[1])
+#ingresar un valor a lista
+dias.append("jueves")
+dias.append("viernes")
+print(dias)
+#eliminar un valor de la lista 
+dias.pop(0)
+print(dias)
+print(dias[1:3])
