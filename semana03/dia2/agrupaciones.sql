5a6885971e0ef9fcbe8c1e5387054cf03fb5b983
Yessica Zegarra <yessizegarrav@hotmail.com>
2024-07-15 19:24:39 -0500

    cambios

---
+++ semana03/dia2/agrupaciones.sql
@@ -0,0 +1,11 @@
+select count(*) from empleado;
+select max(salario),min(salario),avg(salario) from empleado;
+
+select distinct area from empleado;
+select area,avg(salario)
+from empleado 
+group by area
+order by max(salario) desc
+
+select * from empleado 
+where salario > (select avg(salario) from empleado);
